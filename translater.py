5d17fac7ca48d3e4767f1d1819adfc469e7ae090
atzu6237 <atzu6237@gmail.com>
2020-12-11 08:24:12 +0900

    implemented doctest

--- translater.py
+++ translater.py
@@ -1,10 +1,29 @@
 from googletrans import Translator
 
-def translate(inputstr: str, dest: str = 'ja') -> str:
-    translator = Translator()
-    result = translator.translate(inputstr, dest)
+def translate(inputstr: str, dest: str) -> str:
+    t = Translator()
+    result = t.translate(inputstr, dest)
     return result.text
 
+def detect_lang(inputstr: str) -> str:
+    """                                                                            return 'ja'                                                                     >>> detect_lang('こんにちは')                                     
+    'ja'                                                                           """
+    t = Translator()
+    result = t.detect(inputstr)
+    return result.lang
+
+
+def get_lang_ja():
+    """
+    return 'ja'
+    
+    >>> detect_lang('こんにちは')
+    'ja'
+    """
+
 if __name__ == '__main__':
+    
+    print(detect_lang('こんにちは'))
+
     print(translate(inputstr='こんにちは', dest='en'))
     print(translate('Hello', dest='ja'))
