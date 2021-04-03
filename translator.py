058dcec4be660b4164055f9c37b819af6170d6ee
atzu6 <atzu6@DESKTOP-LHS0IN6.mshome.net>
2021-04-03 09:40:42 +0900

    Fix typos
    Delete unnecessary spaces

--- translator.py
+++ translator.py
@@ -6,8 +6,11 @@ def translate(inputstr: str, dest: str) -> str:
     return result.text
 
 def detect_lang(inputstr: str) -> str:
-    """                                                                            return 'ja'                                                                     >>> detect_lang('こんにちは')                                     
-    'ja'                                                                           """
+    """
+    return 'ja'
+    >>> detect_lang('こんにちは')
+    'ja'
+    """
     t = Translator()
     result = t.detect(inputstr)
     return result.lang
@@ -16,13 +19,13 @@ def detect_lang(inputstr: str) -> str:
 def get_lang_ja():
     """
     return 'ja'
-    
+
     >>> detect_lang('こんにちは')
     'ja'
     """
 
 if __name__ == '__main__':
-    
+
     print(detect_lang('こんにちは'))
 
     print(translate(inputstr='こんにちは', dest='en'))
